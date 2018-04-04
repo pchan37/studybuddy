85fff267c36ddc1bfa9cafe3c4f74633f9116454
Patrick Chan <patrickchan1999@gmail.com>
2018-04-04 10:41:06 -0400

    Add 404 page not found handling

--- src/views/publicViews.go
+++ src/views/publicViews.go
@@ -8,10 +8,18 @@ import (
 )
 
 func RegisterPublicViews() {
+	http.HandleFunc("/page_not_found", NotFoundPage)
 	http.HandleFunc("/", security.Authenticate(IndexPage))
 }
 
+func NotFoundPage(w http.ResponseWriter, r *http.Request) {
+	templateManager.RenderTemplate(w, "404.tmpl", nil)
+}
+
 func IndexPage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/page_not_found", http.StatusTemporaryRedirect)
+	}
 	data := map[string]bool{"IsLoggedIn": security.IsLoggedIn(w, r)}
 	templateManager.RenderTemplate(w, "index.tmpl", data)
 }
